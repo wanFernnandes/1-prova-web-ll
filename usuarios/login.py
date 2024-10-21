e16347c0c269b8e9b8bad64917917dd62f97da1b
wanFernnandes <wandersonfernandes856@gmail.com>
2024-10-21 09:18:04 -0300

    Delete usuarios directory

--- usuarios/login.py
+++
@@ -1,7 +0,0 @@
-# forms.py
-from django import forms
-from django.contrib.auth.forms import AuthenticationForm
-
-class CustomLoginForm(AuthenticationForm):
-    username = forms.CharField(max_length=150, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Usuário'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Senha'}))
