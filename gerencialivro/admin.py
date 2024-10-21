d9c7515c8a1e8989212aae94f060fd3ec69e719e
wanFernnandes <wandersonfernandes856@gmail.com>
2024-10-21 09:17:50 -0300

    Delete gerencialivro directory

--- gerencialivro/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from .models import Livro
-
-admin.site.register(Livro)
--- gerencialivro/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Livro
-
-class LivroForm(forms.ModelForm):
-    class Meta:
-        model = Livro
-        fields = ['id_livros', 'autor', 'titulo', 'ano', 'genero']
