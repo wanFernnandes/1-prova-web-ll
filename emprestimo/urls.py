0bab9e8ef8923cd28dfa33845d45966c4f92772a
wanFernnandes <wandersonfernandes856@gmail.com>
2024-11-18 07:42:43 -0300

    Add files via upload

--- emprestimo/urls.py
+++ emprestimo/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
 # Exemplo de URL com o nome correto do parâmetro
-    path('nova/<str:livro_id_livros>/', views.nova_alocacao, name='nova_alocacao'),
+
+    path('nova/<int:livro_id>/', views.nova_alocacao, name='nova_alocacao'),
     path('lista/', views.lista_alocacoes, name='lista_alocacoes'),
     path('finalizar/<int:alocacao_id>/', views.finalizar_alocacao, name='finalizar_alocacao'),
     path('exportar_pdf/', views.exportar_alocacoes_pdf, name='exportar_alocacoes_pdf'),  # Adicione esta linha
--- emprestimo/views.py
+++ emprestimo/views.py
@@ -9,14 +9,24 @@ from django.http import HttpResponse
 from django.template.loader import render_to_string
 from weasyprint import HTML
 
-def nova_alocacao(request, livro_id_livros):
-    livro = get_object_or_404(Livro, id_livros=livro_id_livros)  # Aqui usamos id_livros
-    # Outras lógicas para criar a alocação
-    return render(request, 'nova_alocacao.html', {'livro': livro})
+def nova_alocacao(request, livro_id):
+    livro = get_object_or_404(Livro, pk=livro_id)
+    print(f"Livro encontrado: {livro.titulo}")  # Verifique se isso é impresso no console
+    return render(request, 'emprestimo/nova_alocacao.html', {'livro': livro})
+
+    
+
+from django.shortcuts import render
+from .models import Livro, Alocacao
 
 def lista_alocacoes(request):
-    alocacoes = Alocacao.objects.filter(ativo=True)
-    return render(request, 'emprestimo/lista_alocacoes.html', {'alocacoes': alocacoes})
+    livros = Livro.objects.all()  # Carrega todos os livros para exibir no template
+    alocacoes = Alocacao.objects.filter(ativo=True)  # Filtra as alocações ativas
+    return render(request, 'emprestimo/lista_alocacoes.html', {
+        'livros': livros,
+        'alocacoes': alocacoes,
+    })
+
 
 def finalizar_alocacao(request, alocacao_id):
     alocacao = get_object_or_404(Alocacao, id=alocacao_id)
