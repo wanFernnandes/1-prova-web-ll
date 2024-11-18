ca8d5cf40e7cceee24baa25a8a7c95ecbc90cf01
wanFernnandes <wandersonfernandes856@gmail.com>
2024-11-18 15:57:33 -0300

    Add files via upload

--- biblioteca_v2.2 ATUALIZADO/gerencialivro/models.py
+++ biblioteca_v2.2 ATUALIZADO/gerencialivro/models.py
@@ -6,6 +6,8 @@ class Livro(models.Model):
     ano = models.IntegerField()
     genero = models.CharField(max_length=100)
     id_livros = models.CharField(max_length=15, unique=True)
+    quantidade = models.IntegerField(default=0)
+
 
     def __str__(self):
         return f'{self.autor} - {self.id_livros} - {self.titulo} - {self.ano}'
