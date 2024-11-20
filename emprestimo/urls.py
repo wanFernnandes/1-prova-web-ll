774c066f358db05793f82ed156b91a8a58b166d9
wanFernnandes <wandersonfernandes856@gmail.com>
2024-11-20 18:45:41 -0300

    Add files via upload

--- emprestimo/urls.py
+++ emprestimo/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
 
     path('nova/<int:livro_id>/', views.nova_alocacao, name='nova_alocacao'),
     path('lista/', views.lista_alocacoes, name='lista_alocacoes'),
-    path('finalizar/<int:alocacao_id>/', views.finalizar_alocacao, name='finalizar_alocacao'),
+    path('finalizar/<int:id>/', views.finalizar_alocacao, name='finalizar_alocacao'),
     path('exportar_pdf/', views.exportar_alocacoes_pdf, name='exportar_alocacoes_pdf'),  # Adicione esta linha
 
 ]
